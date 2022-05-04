4160facaeb22e12a8d237a2bb0e7305dffba826b
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-05-04 14:18:08 +0300

    basic elements created, storybook added

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ItemList = (props) => {
+  return <div>ItemList</div>;
+};
+
+ItemList.propTypes = {};
+
+export default ItemList;
---
+++ src/components/StatisticList/StatisticList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StatisticList = (props) => {
+  return <div>StatisticList</div>;
+};
+
+StatisticList.propTypes = {};
+
+export default StatisticList;
